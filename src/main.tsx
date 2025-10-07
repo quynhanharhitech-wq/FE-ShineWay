113489be1f53d52040ac70863ad0a55fa814daef
unknown <toan9370@donga.edu.vn>
2025-10-07 12:11:23 +0700

    change-ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.tsx";
 import "./index.css";
-
 import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider
       theme={{
         token: {
-          // Tùy chỉnh theme Antd nếu cần (ví dụ: màu primary)
-          colorPrimary: "#3b82f6",
+          colorPrimary: "#3b82f6", // Tùy chỉnh màu primary
         },
       }}
     >
